cb4970f8505f4761480ea501aab5e7de51f0e865
Ranjan14 <63440245+Ranjan14@users.noreply.github.com>
2020-09-30 16:25:04 +0530

    method

---
+++ method.java
@@ -0,0 +1,10 @@
+public class method {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        met();    
+    }
+
+    public static void met(){
+        System.out.println("World!");
+    }
+}
