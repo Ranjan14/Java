deca8c47c6b8706c365d76a43624ccbd4399f719
Ranjan14 <63440245+Ranjan14@users.noreply.github.com>
2020-10-29 21:13:21 +0530

    scanner

---
+++ scan.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class scan{
+    public static void main(String[] args){
+        System.out.print("Enter an input ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Name is " + input.nextLine());
+        // .nextLine() reads a String
+        // .next() reads a first word of a string
+        // .nextBoolean() reads a boolean value from the user
+        // .nextByte() reads a Byte
+        // .nextDouble() reads a Double
+        // .nextFloat() reads a Float
+        // .nextInt() reads a Int
+        // .nextLong() reads a Long
+        // .nextShort() reads a Short
+
+        // import java.util.Scanner
+    }
+}
